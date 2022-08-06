4ca8ca3e92ac6005c47309af0edf527a7490d91d
vanluijt <vl54@ziggo.nl>
2022-08-06 21:00:09 +0200

    Initial Commit

---
+++ githubproef/githubproefApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubproefApp.swift
+//  githubproef
+//
+//  Created by P.A. van Luijt on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubproefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
